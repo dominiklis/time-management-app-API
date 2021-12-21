7398366964deb7f346a736e5f0284b03c3477216
dominiklis <lis.dominik11@gmail.com>
2021-12-21 18:38:17 +0100

    reorganize routes in app.js

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const { usersRouter, tasksRouter, projectsRouter } = require("./routes");
-
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const routes = require("./routes");
+
 const handleErrors = require("./middleware/handleErrors");
-const auth = require("./middleware/authMidlleware");
 
 // middleware
 app.use(express.json());
@@ -19,11 +18,8 @@ app.use(cors());
 app.use(helmet());
 
 // routes
-const apiRoute = process.env.API_ROUTE || "/api";
-
-app.use(`${apiRoute}/users`, usersRouter);
-app.use(`${apiRoute}/tasks`, auth, tasksRouter);
-app.use(`${apiRoute}/projects`, auth, projectsRouter);
+const apiRoute = process.env.API_ROUTE || "api";
+app.use(`/${apiRoute}`, routes);
 
 // error handling
 app.use(handleErrors);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/authMidlleware");
+
 const usersRouter = require("./usersRoutes");
 const tasksRouter = require("./tasksRouter");
 const projectsRouter = require("./projectsRouter");
 
-module.exports = {
-  usersRouter,
-  tasksRouter,
-  projectsRouter,
-};
+router.use("/users", usersRouter);
+router.use("/tasks", auth, tasksRouter);
+router.use("/projects", auth, projectsRouter);
+
+module.exports = router;
